08b2613ef661aaee343a979ec53f51d0d87d838f
ABDNerwat <abhinerwa@gmail.com>
2024-03-10 20:57:07 +0530

    Add sketchpad with one color functionality and clear button

--- main.js
+++ main.js
@@ -1,17 +1,118 @@
 let sid = document.getElementById("side").value;
-const sketch = document.querySelector("#sketch")
+let cells = document.querySelectorAll(".cell");
+let erase = false;
+console.log(cells);
+let drawState=false;
 
-for (let i = 0; i < sid; i++){
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.setAttribute("style", "display:flex; flex:1");
-    for (let j = 0; j < sid; j++){
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.setAttribute("style", "flex:1; background-color:white");
-        grid.appendChild(cell);
+side.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13){
+        let erase = false;
+        const sketch = document.querySelector("#sketch");
+        for (let i = 0; i < sid; i++){
+            sketch.removeChild(sketch.lastElementChild);
+        }
+        sid = document.getElementById("side").value;
+        console.log(sid);
+        for (let i = 0; i < sid; i++){
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.setAttribute("style", "display:flex; flex:1");
+            for (let j = 0; j < sid; j++){
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.setAttribute("style", "flex:1; background-color:white");
+                grid.appendChild(cell);
+            }
+            sketch.appendChild(grid);
+        }
+        cells = document.querySelectorAll(".cell");
+        cells.forEach((item) => {
+            item.addEventListener("mouseenter", () =>{
+                if (!erase && drawState){ 
+                    item.setAttribute("style", "background-color:black");
+                }
+                else if (drawState){
+                    item.setAttribute("style", "background-color:white");
+                }
+            });
+            item.addEventListener("mousedown", () => {
+                if (!erase){ 
+                    item.setAttribute("style", "background-color:black");
+                }
+                else{
+                    item.setAttribute("style", "background-color:white");
+                }
+            })
+        });
     }
-    sketch.appendChild(grid);
-    console.log(sketch.querySelectorAll("div").length);
-}
+})
 
+sketch.addEventListener("mousedown", () => {
+    drawState = true;
+});
+
+sketch.addEventListener("mouseup", () =>{
+    drawState = false;
+});
+
+sketch.addEventListener("mouseenter", () => {drawState = false;});
+
+cells.forEach((item) => {
+    item.addEventListener("mouseenter", () =>{
+        if (!erase && drawState){ 
+            item.setAttribute("style", "background-color:black");
+        }
+        else if (drawState){
+            item.setAttribute("style", "background-color:white");
+        }
+    });
+    item.addEventListener("mousedown", () => {
+        if (!erase){ 
+            item.setAttribute("style", "background-color:black");
+        }
+        else{
+            item.setAttribute("style", "background-color:white");
+        }
+    })
+});
+
+clear.addEventListener("click", () =>{
+    let erase = false;
+        const sketch = document.querySelector("#sketch");
+        for (let i = 0; i < sid; i++){
+            sketch.removeChild(sketch.lastElementChild);
+        }
+        sid = document.getElementById("side").value;
+        console.log(sid);
+        for (let i = 0; i < sid; i++){
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.setAttribute("style", "display:flex; flex:1");
+            for (let j = 0; j < sid; j++){
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.setAttribute("style", "flex:1; background-color:white");
+                grid.appendChild(cell);
+            }
+            sketch.appendChild(grid);
+        }
+        cells = document.querySelectorAll(".cell");
+        cells.forEach((item) => {
+            item.addEventListener("mouseenter", () =>{
+                if (!erase && drawState){ 
+                    item.setAttribute("style", "background-color:black");
+                }
+                else if (drawState){
+                    item.setAttribute("style", "background-color:white");
+                }
+            });
+            item.addEventListener("mousedown", () => {
+                if (!erase){ 
+                    item.setAttribute("style", "background-color:black");
+                }
+                else{
+                    item.setAttribute("style", "background-color:white");
+                }
+            })
+        });
+})
